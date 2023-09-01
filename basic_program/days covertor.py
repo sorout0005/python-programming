762a2b583c8b0de81dc8c45fe8b6bdfce09e1d76
gautam sorout <142593022+sorout0005@users.noreply.github.com>
2023-09-01 21:01:39 +0530

    Add files via upload

---
+++ basic_program/days covertor.py
@@ -0,0 +1,4 @@
+a=int(input())
+b=a//7
+print('days', a%7)
+print('weeks', b)
---
+++ basic_program/temprature covertor.py
@@ -0,0 +1,5 @@
+t=float(input("enter the temprature in fahrenheit"))
+ftc=(t-32)*5/9
+ftk=(t-32)*5/9+273.15
+print("your temprature in celsius is", ftc)
+print("your temprature in kelvin is", ftk)
